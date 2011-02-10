001210f32f9a574906371c68b287b92761e7af58
dbu <david.buchmann@liip.ch>
2011-02-10 13:56:29 +0100

    adding the frontend skeleton

--- src/Liip/HackdayBundle/Controller/DefaultController.php
+++ src/Liip/HackdayBundle/Controller/DefaultController.php
@@ -6,8 +6,41 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    /**
+     * @var \Bundle\DoctrinePHPCRBundle\JackalopeLoader
+     */
+    protected $jackalope = null;
+
+    /**
+     * @var \Doctrine\ODM\PHPCR\DocumentManager
+     */
+    protected $dm = null;
+
+    public function __construct($container, $jackalope, $dm)
     {
-        return $this->render('HackdayBundle:Default:index.html.twig');
+        $this->setContainer($container);
+        $this->jackalope = $jackalope;
+        $this->dm = $dm;
+    }
+
+    public function indexAction($path)
+    {
+        return $this->render('HackdayBundle:Default:index.html.twig', array('path'=>$path));
+    }
+
+    /**
+     * render the document identified by path
+     */
+    public function contentAction($path)
+    {
+        return $this->render('HackdayBundle:Default:document.html.twig', array('path'=>$path));
+    }
+
+    /**
+     * render a list of children of the node identified by path
+     */
+    public function childlistAction($path)
+    {
+        return $this->render('HackdayBundle:Default:childlist.html.twig', array('path'=>$path));
     }
 }
